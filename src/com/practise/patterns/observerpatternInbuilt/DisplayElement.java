219ca82dda3a8e206d3d107e045f4f1b9f6318bd
trinath <tod3.3nath@gmail.com>
2017-02-24 00:23:34 +0530

    Observer Pattern using Inbuilt Observer in Java

---
+++ src/com/practise/patterns/observerpatternInbuilt/DisplayElement.java
@@ -0,0 +1,6 @@
+// Source HeadFirstDesignPatterns Book
+package com.practise.patterns.observerpatternInbuilt;
+
+public interface DisplayElement {
+    public void display();
+}
---
+++ src/com/practise/patterns/observerpatternInbuilt/WeatherStation.java
@@ -0,0 +1,12 @@
+// Source HeadFirstDesignPatterns Book
+package com.practise.patterns.observerpatternInbuilt;
+
+public class WeatherStation {
+    public static void main(String[] args){
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherData);
+        weatherData.setMeasurements(10, 20, 30);
+        weatherData.setMeasurements(13, 14, 15);
+        weatherData.setMeasurements(16, 24, 35);
+    }
+}
