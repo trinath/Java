f67223407b7d8ead722396008a02cff25ef358eb
trinath <tod3.3nath@gmail.com>
2017-06-12 11:06:07 +0530

    Decorator Pattern

--- src/com/practise/patterns/simplefactorypattern/ClamPizza.java
+++ src/com/practise/patterns/simplefactorypattern/ClamPizza.java
@@ -3,15 +3,15 @@ package com.practise.patterns.simplefactorypattern;
 
 public class ClamPizza extends Pizza {
     public void prepare(){
-
+        System.out.println("Preparing Clam Pizza");
     }
     public void bake(){
-
+        System.out.println("Baking Clam Pizza");
     }
     public void cut(){
-
+        System.out.println("Cutting Clam Pizza");
     }
     public void box(){
-
+        System.out.println("Adding Clam Pizza to Box");
     }
 }
--- src/com/practise/patterns/simplefactorypattern/VeggiePizza.java
+++ src/com/practise/patterns/simplefactorypattern/VeggiePizza.java
@@ -3,15 +3,15 @@ package com.practise.patterns.simplefactorypattern;
 
 public class VeggiePizza extends Pizza {
     public void prepare(){
-
+        System.out.println("Preparing Veggie Pizza");
     }
     public void bake(){
-
+        System.out.println("Baking Veggie Pizza");
     }
     public void cut(){
-
+        System.out.println("Cutting Veggie Pizza");
     }
     public void box(){
-
+        System.out.println("Adding Veggie Pizza to Box");
     }
 }
